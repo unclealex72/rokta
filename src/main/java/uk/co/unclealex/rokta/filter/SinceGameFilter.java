6b6a5cdaf0e51076bd87720bffb2123c46dace4f
Alex Jones <alex.jones@unclealex.co.uk>
2007-02-03 19:36:24 +0000

    Fixed the since game filter.

--- src/main/java/uk/co/unclealex/rokta/filter/SinceGameFilter.java
+++ src/main/java/uk/co/unclealex/rokta/filter/SinceGameFilter.java
@@ -1,30 +1,22 @@
 package uk.co.unclealex.rokta.filter;
 
-import java.util.Date;
-
 import uk.co.unclealex.rokta.process.restriction.GameRestriction;
 import uk.co.unclealex.rokta.process.restriction.SinceGameRestriction;
 
 public class SinceGameFilter extends SingleDateGameRestrictionGameFilter {
 
-	private Date i_since;
-	
 	@Override
 	protected char getEncodingPrefix() {
-		return GameFilterFactory.BEFORE_PREFIX;
+		return GameFilterFactory.SINCE_PREFIX;
 	}
 
 	@Override
 	protected GameRestriction getGameRestriction() {
-		return new SinceGameRestriction(getSince());
+		return new SinceGameRestriction(getDate());
 	}
 
 	public String getDescription() {
-		return "since " + createDisplayDateFormat().format(getSince());
-	}
-
-	public Date getSince() {
-		return i_since;
+		return "since " + createDisplayDateFormat().format(getDate());
 	}
 
 	public void accept(GameFilterVistor gameFilterVisitor) {
