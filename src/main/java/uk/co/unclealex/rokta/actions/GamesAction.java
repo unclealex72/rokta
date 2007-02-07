d5c8071d2e855eed6c959dffefd54c417a9a0a18
Alex Jones <alex.jones@unclealex.co.uk>
2007-02-07 19:55:13 +0000

    The games page works again.

--- src/main/java/uk/co/unclealex/rokta/actions/GamesAction.java
+++ src/main/java/uk/co/unclealex/rokta/actions/GamesAction.java
@@ -1,16 +1,18 @@
 package uk.co.unclealex.rokta.actions;
 
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 import org.apache.commons.collections15.ComparatorUtils;
 
 import uk.co.unclealex.rokta.model.Game;
+import uk.co.unclealex.rokta.views.GameView;
 
 public class GamesAction extends RoktaAction {
 
-	private SortedSet<Game> i_reversedGames;
+	private Iterator<GameView> i_gameViews;
 	
 	@Override
 	public String executeInternal() {
@@ -18,15 +20,29 @@ public class GamesAction extends RoktaAction {
 			ComparatorUtils.reversedComparator(ComparatorUtils.NATURAL_COMPARATOR);
 		SortedSet<Game> reversedGames = new TreeSet<Game>(reverseComparator);
 		reversedGames.addAll(getGames());
-		setReversedGames(reversedGames);
+		final Iterator<Game> gameIter = reversedGames.iterator();
+		final Iterator<GameView> iter = new Iterator<GameView>() {
+			public boolean hasNext() {
+				return gameIter.hasNext();
+			}
+
+			public GameView next() {
+				return new GameView(gameIter.next());
+			}
+
+			public void remove() {
+			}
+		};
+		setGameViews(iter);
 		return SUCCESS;
 	}
-	
-	public SortedSet<Game> getReversedGames() {
-		return i_reversedGames;
+
+	public Iterator<GameView> getGameViews() {
+		return i_gameViews;
 	}
 
-	public void setReversedGames(SortedSet<Game> reversedGames) {
-		i_reversedGames = reversedGames;
+	public void setGameViews(Iterator<GameView> gameViews) {
+		i_gameViews = gameViews;
 	}
+
 }
