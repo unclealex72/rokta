6da214330b247c44889defbb908e963173a9fa39
Alex Jones <alex.jones@unclealex.co.uk>
2009-03-22 12:30:35 +0000

    Creating a new Rokta frontend using GWT.

---
+++ gwt/src/test/java/uk/co/unclealex/rokta/gwt/client/GwtTestExampleA.java
@@ -0,0 +1,19 @@
+package uk.co.unclealex.rokta.gwt.client;
+
+import junit.framework.Assert;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+public class GwtTestExampleA extends GWTTestCase {
+
+   public String getModuleName() {
+      return "uk.co.unclealex.rokta.gwt.Application";
+   }
+
+   public void testSomething() {
+      // Not much to actually test in this sample app
+      // Ideally you would test your Controller here (NOT YOUR UI)
+      // (Make calls to RPC services, test client side model objects, test client side logic, etc)
+      Assert.assertTrue(true);
+   }
+}
