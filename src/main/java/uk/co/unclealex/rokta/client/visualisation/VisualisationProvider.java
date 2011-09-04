0a841979de6a084f8262875b3f46324d831864a5
Alex Jones <alex.jones@unclealex.co.uk>
2011-09-04 14:45:26 +0000

    Removed visualisation.

--- src/main/java/uk/co/unclealex/rokta/client/visualisation/VisualisationProvider.java
+++
@@ -1,9 +0,0 @@
-package uk.co.unclealex.rokta.client.visualisation;
-
-import com.google.gwt.visualization.client.DataTable;
-
-public interface VisualisationProvider {
-
-	DataTable createDataTable();
-
-}
