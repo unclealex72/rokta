892f67befd0cf221491edfa6e23b64c1698d98ec
Alex Jones <alex.jones@unclealex.co.uk>
2013-10-18 18:59:38 +0100

    Add final decorator test for leagues

--- test/stats/LeagueFactoryImplSpec.scala
+++ test/stats/LeagueFactoryImplSpec.scala
@@ -46,158 +46,190 @@ class LeagueFactoryImplSpec extends Specification with Mockito {
    * Give tests access to the concrete [[LeagueFactoryImpl]] and mocked [[GapCalculator]]
    */
   def mocked[B](block: LeagueFactoryImpl => GapCalculator => B) = {
-    val gapCalculator = mock[GapCalculator]
+    val gapCalculator: GapCalculator = mock[GapCalculator]
     implicit val module = new NewBindingModule({ module =>
       import module._
-      bind [GapCalculator] toSingle gapCalculator
+      bind[GapCalculator] toSingle gapCalculator
     })
     val leagueFactory = new LeagueFactoryImpl()
     block(leagueFactory)(gapCalculator)
   }
-  
-  "A non-current league" should {
-    "not be decorated" in mocked { (leagueFactory: LeagueFactoryImpl) => (gapCalculator: GapCalculator) =>
-      def snapshots = 
-        on(
-          October(15, 2013) at (11 oclock),
-          "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12),
-          "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
-          "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12),
-          "John".wins(2).playingRounds(12).loses(2).playingRounds(12)) ++
-        on(
-          October(15, 2013) at (15 oclock),
-          "John".wins(5).playingRounds(12).loses(1).playingRounds(12),
-          "Freddie".wins(4).playingRounds(12).loses(2).playingRounds(12),
-          "Roger".wins(3).playingRounds(12).loses(3).playingRounds(12),
-          "Brian".wins(2).playingRounds(12).loses(2).playingRounds(12))
-        
-      val league = leagueFactory.generateLeague(snapshots, None, None)
-      league must contain { (leagueRow: LeagueRow) => 
-        (leagueRow.currentlyPlaying must beFalse) and
-        (leagueRow.movement must beNone) and
-        (leagueRow.exempt must beFalse) and
-        (leagueRow.gap must beNone)
-      }.foreach        
-    }
-  }  
 
-  "A league generated from a single snapshot" should {
-    "not be decorated" in mocked { (leagueFactory: LeagueFactoryImpl) => (gapCalculator: GapCalculator) =>
-      def snapshots = 
-        on(
-          October(15, 2013) at (11 oclock),
-          "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12),
-          "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
-          "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12),
-          "John".wins(2).playingRounds(12).loses(2).playingRounds(12))
-          
-      val league = leagueFactory.generateLeague(snapshots, None, None)
-      league must contain { (leagueRow: LeagueRow) => 
-        (leagueRow.currentlyPlaying must beFalse) and
-        (leagueRow.movement must beNone) and
-        (leagueRow.exempt must beFalse) and
-        (leagueRow.gap must beNone)
-      }.foreach        
+  "A non-current league" should {
+    "not be decorated" in mocked { (leagueFactory: LeagueFactoryImpl) =>
+      (gapCalculator: GapCalculator) =>
+        def snapshots =
+          on(
+            October(15, 2013) at (11 oclock),
+            "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12),
+            "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
+            "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12),
+            "John".wins(2).playingRounds(12).loses(2).playingRounds(12)) ++
+            on(
+              October(15, 2013) at (15 oclock),
+              "John".wins(5).playingRounds(12).loses(1).playingRounds(12),
+              "Freddie".wins(4).playingRounds(12).loses(2).playingRounds(12),
+              "Roger".wins(3).playingRounds(12).loses(3).playingRounds(12),
+              "Brian".wins(2).playingRounds(12).loses(2).playingRounds(12))
+
+        val league = leagueFactory.generateLeague(snapshots, None, None)
+        league must contain { (leagueRow: LeagueRow) =>
+          (leagueRow.currentlyPlaying must beFalse) and
+            (leagueRow.movement must beNone) and
+            (leagueRow.exempt must beFalse) and
+            (leagueRow.gap must beNone)
+        }.foreach
     }
   }
-  
+
+  "A league generated from a single snapshot" should {
+    "not be decorated" in mocked { (leagueFactory: LeagueFactoryImpl) =>
+      (gapCalculator: GapCalculator) =>
+        def snapshots =
+          on(
+            October(15, 2013) at (11 oclock),
+            "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12),
+            "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
+            "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12),
+            "John".wins(2).playingRounds(12).loses(2).playingRounds(12))
+
+        val league = leagueFactory.generateLeague(snapshots, None, None)
+        league must contain { (leagueRow: LeagueRow) =>
+          (leagueRow.currentlyPlaying must beFalse) and
+            (leagueRow.movement must beNone) and
+            (leagueRow.exempt must beFalse) and
+            (leagueRow.gap must beNone)
+        }.foreach
+    }
+  }
+
   "Decorating a league with who is currently playing and who is currently not" should {
-    "mark playing players' results as current" in mocked { 
-      (leagueFactory: LeagueFactoryImpl) => (gapCalculator: GapCalculator) =>
-        val nonDecoratedLeague = SortedSet[LeagueRow](
-          "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12),
-          "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
-          "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12),
-          "John".wins(2).playingRounds(12).loses(2).playingRounds(12))
+    "mark playing players' results as current" in mocked {
+      (leagueFactory: LeagueFactoryImpl) =>
+        (gapCalculator: GapCalculator) =>
+          val nonDecoratedLeague = SortedSet[LeagueRow](
+            "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12),
+            "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
+            "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12),
+            "John".wins(2).playingRounds(12).loses(2).playingRounds(12))
 
-        val decoratedLeague = SortedSet[LeagueRow](
-          "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12).withCurrentlyPlaying(true),
-          "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
-          "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12).withCurrentlyPlaying(true),
-          "John".wins(2).playingRounds(12).loses(2).playingRounds(12))
+          val decoratedLeague = SortedSet[LeagueRow](
+            "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12).withCurrentlyPlaying(true),
+            "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
+            "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12).withCurrentlyPlaying(true),
+            "John".wins(2).playingRounds(12).loses(2).playingRounds(12))
 
-        leagueFactory.decorateWithCurrent(players("Brian", "Freddie"))(nonDecoratedLeague) must be equalTo(decoratedLeague)
-    }    
+          leagueFactory.decorateWithCurrent(players("Brian", "Freddie"))(nonDecoratedLeague) must be equalTo (decoratedLeague)
+    }
   }
 
   "Decorating a league with who is currently exempt" should {
-    "mark exempt players' results as exempt" in mocked { 
-      (leagueFactory: LeagueFactoryImpl) => (gapCalculator: GapCalculator) =>
-        val nonDecoratedLeague = SortedSet[LeagueRow](
-          "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12).withCurrentlyPlaying(true),
-          "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
-          "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12).withCurrentlyPlaying(true),
-          "John".wins(2).playingRounds(12).loses(2).playingRounds(12))
+    "mark exempt players' results as exempt" in mocked {
+      (leagueFactory: LeagueFactoryImpl) =>
+        (gapCalculator: GapCalculator) =>
+          val nonDecoratedLeague = SortedSet[LeagueRow](
+            "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12).withCurrentlyPlaying(true),
+            "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
+            "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12).withCurrentlyPlaying(true),
+            "John".wins(2).playingRounds(12).loses(2).playingRounds(12))
 
-        val decoratedLeague = SortedSet[LeagueRow](
-          "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12).withCurrentlyPlaying(true).withExempt(true),
-          "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
-          "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12).withCurrentlyPlaying(true),
-          "John".wins(2).playingRounds(12).loses(2).playingRounds(12))
+          val decoratedLeague = SortedSet[LeagueRow](
+            "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12).withCurrentlyPlaying(true).withExempt(true),
+            "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
+            "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12).withCurrentlyPlaying(true),
+            "John".wins(2).playingRounds(12).loses(2).playingRounds(12))
 
-        leagueFactory.decorateWithExemption(Some(player("Brian")))(nonDecoratedLeague) must be equalTo(decoratedLeague)
+          leagueFactory.decorateWithExemption(Some(player("Brian")))(nonDecoratedLeague) must be equalTo (decoratedLeague)
     }
   }
-  
+
   "Decorating a league with the gaps between players" should {
-    "compare the bottom three players to the players above them" in mocked { 
-      (leagueFactory: LeagueFactoryImpl) => (gapCalculator: GapCalculator) =>
-        val brian: LeagueRow = "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12).withCurrentlyPlaying(true)
-        val roger: LeagueRow = "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12).withCurrentlyPlaying(true)
-        val freddie: LeagueRow = "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12)
-        val john: LeagueRow = "John".wins(2).playingRounds(12).loses(2).playingRounds(12)
-      gapCalculator.calculateGap(brian, roger) returns (Some(1))
-      gapCalculator.calculateGap(roger, freddie) returns (Some(2))
-      gapCalculator.calculateGap(freddie, john) returns (Some(3))
-      val decoratedLeague = 
-        leagueFactory.decorateWithGap(players("Brian", "Roger", "Freddie", "John"))(SortedSet(brian, roger, freddie, john))
-      decoratedLeague must contain(brian, roger.withGap(1), freddie.withGap(2), john.withGap(3)).inOrder
+    "compare the bottom three players to the players above them" in mocked {
+      (leagueFactory: LeagueFactoryImpl) =>
+        (gapCalculator: GapCalculator) =>
+          val brian: LeagueRow = "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12).withCurrentlyPlaying(true)
+          val roger: LeagueRow = "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12).withCurrentlyPlaying(true)
+          val freddie: LeagueRow = "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12)
+          val john: LeagueRow = "John".wins(2).playingRounds(12).loses(2).playingRounds(12)
+          gapCalculator.calculateGap(brian, roger) returns (Some(1))
+          gapCalculator.calculateGap(roger, freddie) returns (Some(2))
+          gapCalculator.calculateGap(freddie, john) returns (Some(3))
+          val decoratedLeague =
+            leagueFactory.decorateWithGap(players("Brian", "Roger", "Freddie", "John"))(SortedSet(brian, roger, freddie, john))
+          decoratedLeague must contain(brian, roger.withGap(1), freddie.withGap(2), john.withGap(3)).inOrder
+    }
+  }
+
+  "Generating a full league" should {
+    "correctly decorate each row" in mocked {
+      (leagueFactory: LeagueFactoryImpl) =>
+        (gapCalculator: GapCalculator) =>
+          def snapshots =
+            on(
+              October(15, 2013) at (11 oclock),
+              "Brian".wins(5).playingRounds(12).loses(1).playingRounds(12),
+              "Roger".wins(4).playingRounds(12).loses(2).playingRounds(12),
+              "Freddie".wins(3).playingRounds(12).loses(3).playingRounds(12),
+              "John".wins(2).playingRounds(12).loses(2).playingRounds(12)) ++
+              on(
+                October(15, 2013) at (15 oclock),
+                "John".wins(5).playingRounds(12).loses(1).playingRounds(12),
+                "Freddie".wins(4).playingRounds(12).loses(2).playingRounds(12),
+                "Roger".wins(3).playingRounds(12).loses(3).playingRounds(12),
+                "Brian".wins(2).playingRounds(12).loses(2).playingRounds(12))
+          gapCalculator.calculateGap(any[LeagueRow], any[LeagueRow]) returns Some(2)
+          def league = leagueFactory.generateLeague(snapshots, Some(players("Brian", "Roger")), Some(player("Freddie")))
+          league must contain(
+            "John".wins(5).playingRounds(12).loses(1).playingRounds(12).withMovement(-3),
+            "Freddie".wins(4).playingRounds(12).loses(2).playingRounds(12).withMovement(-1).withExempt(true).withGap(2),
+            "Roger".wins(3).playingRounds(12).loses(3).playingRounds(12).withCurrentlyPlaying(true).withMovement(1).withGap(2),
+            "Brian".wins(2).playingRounds(12).loses(2).playingRounds(12).withCurrentlyPlaying(true).withMovement(3).withGap(2)).inOrder
     }
   }
 }
 
 object LeagueFactoryImplSpec {
   // DSL for snapshot and league row creation
-  
+
   implicit class PlayerNameImplicits(playerName: String) {
     def wins(gamesWon: Int) = Wins(playerName, gamesWon)
   }
-  
+
   case class Wins(playerName: String, gamesWon: Int) {
-    def playingRounds(roundsPlayedInWinningGames: Int) = 
+    def playingRounds(roundsPlayedInWinningGames: Int) =
       WinsRounds(playerName, gamesWon, roundsPlayedInWinningGames)
   }
-  
+
   case class WinsRounds(playerName: String, gamesWon: Int, roundsPlayedInWinningGames: Int) {
     def loses(gamesLost: Int) = Loses(playerName, gamesWon, roundsPlayedInWinningGames, gamesLost)
   }
-  
+
   case class Loses(playerName: String, gamesWon: Int, roundsPlayedInWinningGames: Int, gamesLost: Int) {
-    def playingRounds(roundsPlayedInLosingGames: Int) = 
+    def playingRounds(roundsPlayedInLosingGames: Int) =
       LeagueRowOrSnapshot(playerName, gamesWon, roundsPlayedInWinningGames, gamesLost, roundsPlayedInLosingGames)
   }
-  
+
   case class LeagueRowOrSnapshot(
     playerName: String, gamesWon: Int, roundsPlayedInWinningGames: Int, gamesLost: Int, roundsPlayedInLosingGames: Int) {
-    
+
     def asLeagueRow = LeagueRow(playerName, gamesWon, gamesLost, roundsPlayedInWinningGames, roundsPlayedInLosingGames)
     def asSnapshot =
-      TestPlayer(playerName).asInstanceOf[Player] -> 
+      TestPlayer(playerName).asInstanceOf[Player] ->
         Snapshot(gamesWon, gamesLost, roundsPlayedInWinningGames, roundsPlayedInLosingGames)
   }
-  
+
   case class TestPlayer(name: String, email: String, colour: Colour) extends Player
   object TestPlayer {
     def apply(name: String): TestPlayer = TestPlayer(name, name, Colour.BLACK)
   }
-  
+
   def player(name: String): Player = TestPlayer(name)
   def players(names: String*): Set[Player] = names.toSet.map(player _)
-  
-  implicit def LeagueRowOrSnapshotToLeagueRow(leagueRowOrSnapshot: LeagueRowOrSnapshot): LeagueRow = 
+
+  implicit def LeagueRowOrSnapshotToLeagueRow(leagueRowOrSnapshot: LeagueRowOrSnapshot): LeagueRow =
     leagueRowOrSnapshot.asLeagueRow
 
-  implicit def LeagueRowOrSnapshotToSnapshot(leagueRowOrSnapshot: LeagueRowOrSnapshot): Pair[Player, Snapshot] = 
+  implicit def LeagueRowOrSnapshotToSnapshot(leagueRowOrSnapshot: LeagueRowOrSnapshot): Pair[Player, Snapshot] =
     leagueRowOrSnapshot.asSnapshot
 
   def on(dateTime: DateTime, snapshots: Pair[Player, Snapshot]*): SortedMap[DateTime, Map[Player, Snapshot]] =
