0835fc48a23cfadffa8d60946d599baf771e4f88
Alex Jones <alex.jones@unclealex.co.uk>
2013-10-21 22:31:48 +0100

    Add date extraction functions to postgresql.

---
+++ conf/evolutions/default/2.sql
@@ -0,0 +1,30 @@
+# Date and time functions
+
+# --- !Ups
+
+CREATE OR REPLACE FUNCTION year("when" timestamp without time zone)
+  RETURNS integer AS
+'select cast(extract(year from $1) as integer);;'
+  LANGUAGE sql VOLATILE
+  COST 100;
+CREATE OR REPLACE FUNCTION month("when" timestamp without time zone)
+  RETURNS integer AS
+'select cast(extract(month from $1) as integer);;'
+  LANGUAGE sql VOLATILE
+  COST 100;
+CREATE OR REPLACE FUNCTION week("when" timestamp without time zone)
+  RETURNS integer AS
+'select cast(extract(week from $1) as integer);;'
+  LANGUAGE sql VOLATILE
+  COST 100;
+CREATE OR REPLACE FUNCTION day("when" timestamp without time zone)
+  RETURNS integer AS
+'select cast(extract(day from $1) as integer);;'
+  LANGUAGE sql VOLATILE
+  COST 100;
+
+# --- !Downs
+DROP FUNCTION year("when" timestamp without time zone);
+DROP FUNCTION month("when" timestamp without time zone);
+DROP FUNCTION day("when" timestamp without time zone);
+DROP FUNCTION week("when" timestamp without time zone);
