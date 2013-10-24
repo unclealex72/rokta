fa22abcd3bb7ba29d4a458eee9601750fde73e60
Alex Jones <alex.jones@unclealex.co.uk>
2013-10-24 21:34:35 +0100

    Get a very simple web app actually running.

---
+++ app/Global.scala
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2013 Alex Jones
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with work for additional information
+ * regarding copyright ownership.  The ASF licenses file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+import play.api.GlobalSettings
+import org.squeryl.internals.DatabaseAdapter
+import org.squeryl.SessionFactory
+import org.squeryl.Session
+import play.api.db.DB
+import com.typesafe.scalalogging.slf4j.Logging
+import play.api.Application
+import org.squeryl.adapters.H2Adapter
+import org.squeryl.adapters.PostgreSqlAdapter
+import com.escalatesoft.subcut.inject.Injectable
+import scala.reflect.ClassTag
+import scala.reflect.api.TypeTags
+import scala.reflect.ManifestFactory
+import com.escalatesoft.subcut.inject.BindingModule
+import controllers.StatsController
+import controllers.StatsController
+
+/**
+ * The [[GlobalSettings]] used to set up Squeryl and Subcut
+ * @author alex
+ *
+ */
+object Global extends GlobalSettings with Logging {
+
+ object Context extends Injectable {
+    implicit val bindingModule = RoktaBindingModule  // use the standard config by default
+
+    val controllers: Map[Class[_], _] = Map(
+      classOf[StatsController] -> inject[StatsController]
+    )
+  }
+
+  /**
+   * Controllers must be resolved through the bindings. There is a special method of GlobalSettings
+   * that we can override to resolve a given controller. This resolution is required by the Play router.
+   */
+  override def getControllerInstance[A](controllerClass: Class[A]): A = {
+    val controller = Context.controllers.get(controllerClass)
+    controller match {
+      case Some(controller) => controller.asInstanceOf[A]
+      case _ => throw new IllegalArgumentException(s"$controllerClass is not a valid controller class.")
+    }
+  }
+  
+  override def onStart(app: Application) {
+    logger info "Setting up database access."
+    // Set up Squeryl database access
+    SessionFactory.concreteFactory = app.configuration.getString("db.default.driver") match {
+      case Some("org.h2.Driver") => Some(() => getSession(new H2Adapter, app))
+      case Some("org.postgresql.Driver") => Some(() => getSession(new PostgreSqlAdapter, app))
+      case _ => sys.error("Database driver must be either org.h2.Driver or org.postgresql.Driver")
+    }
+  }
+
+  def getSession(adapter: DatabaseAdapter, app: Application) = Session.create(DB.getConnection()(app), adapter)
+
+}
---
+++ app/RoktaBindingModule.scala
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2013 Alex Jones
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with work for additional information
+ * regarding copyright ownership.  The ASF licenses file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+
+/**
+ * The main SubCut [[BindingModule]] for this application.
+ * @author alex
+ *
+ */
+import com.escalatesoft.subcut.inject.NewBindingModule
+import com.escalatesoft.subcut.inject.NewBindingModule._
+import stats.SnapshotsFactory
+import stats.SnapshotsFactoryImpl
+import stats.LeagueFactory
+import stats.LeagueFactoryImpl
+import dao.SquerylDao
+import dao.GameDao
+import dao.PlayerDao
+import dao.Transactional
+import dao.SquerylDao
+import stats.StreaksFactory
+import stats.StreaksFactoryImpl
+import stats.RaceGapCalculator
+import stats.GapCalculator
+import controllers.StatsController
+import controllers.StatsController
+
+object RoktaBindingModule extends NewBindingModule(module => {
+  import module._   // can now use bind directly
+
+  // DAOs
+  val squerylDao = new SquerylDao()
+  bind[GameDao] toSingle(squerylDao)
+  bind[PlayerDao] toSingle(squerylDao)
+  bind[Transactional] toSingle(squerylDao)
+  
+  // Stats
+  bind[SnapshotsFactory] toModuleSingle { implicit module => new SnapshotsFactoryImpl }
+  bind[LeagueFactory] toModuleSingle { implicit module => new LeagueFactoryImpl }
+  bind[StreaksFactory] toModuleSingle { implicit module => new StreaksFactoryImpl }
+  bind[GapCalculator] toModuleSingle { implicit module => new RaceGapCalculator }
+  
+  // Controllers
+  bind[StatsController] toModuleSingle { implicit module => new StatsController }
+    })
