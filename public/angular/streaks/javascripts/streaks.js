e74cbd4d625cc6a04e3123d9f1328514ab90b0ff
Alex Jones <alex.jones@unclealex.co.uk>
2013-12-08 10:50:16 +0000

    Streaks are now shown in a panel.

--- public/angular/streaks/javascripts/streaks.js
+++ public/angular/streaks/javascripts/streaks.js
@@ -1,44 +1,50 @@
-var streaks = angular.module('rokta.streaks', ['rokta.stats', 'rokta.events']);
+var streaks = angular.module('rokta.streaks', ['rokta.stats', 'rokta.events', 'rokta.panel']);
 
-streaks.service('Streaks', ['Events', 'Stats', function(Events, Stats) {
+streaks.service('Streaks', ['Events', 'Stats',
+function(Events, Stats) {
   var service = {
-    initialise: function($scope, type, filter) {
+    initialise : function($scope, type, filter) {
       Stats.refresh();
       Events.listenTo($scope, Stats, function() {
-    	var currentSize = null;
-    	var currentIndex = 1;
-    	var view = function(streak) {
-		  var streakSize = streak.dateTimes.length;
-		  var index;
-		  if (streakSize == currentSize) {
-			index = null;
-		  }
-		  else {
-			index = currentIndex;
-		  }
-		  var streakView = {
-			index: index,
-			size: streakSize,
-			name: streak.playerName,
-			from: _.first(streak.dateTimes),
-			to: _.last(streak.dateTimes)
-		  };
-		  currentSize = streakSize;
-		  currentIndex++;
-		  return streakView;
-		};
-    	$scope.streaks = _.map(Stats.stats.streaks[filter], view);
-	    $scope.type = type;
+        var currentSize = null;
+        var currentIndex = 1;
+        var view = function(streak) {
+          var streakSize = streak.dateTimes.length;
+          var index;
+          if (streakSize == currentSize) {
+            index = null;
+          } else {
+            index = currentIndex;
+          }
+          var streakView = {
+            index : index,
+            size : streakSize,
+            name : streak.playerName,
+            from : _.first(streak.dateTimes),
+            to : _.last(streak.dateTimes)
+          };
+          currentSize = streakSize;
+          currentIndex++;
+          return streakView;
+        };
+        var streaks = _(Stats.stats.streaks[filter]).map(view).filter(function(streak, idx) {
+          return idx < 10;          
+        }).value();
+        $scope.streaksCount = streaks.length;
+        $scope.type = type;
+        $scope.streaks = streaks;
       });
-    } 
+    }
   };
   return service;
 }]);
 
-streaks.controller('WinningStreaksCtrl', ['$scope', 'Streaks', function($scope, Streaks) {
+streaks.controller('WinningStreaksCtrl', ['$scope', 'Streaks',
+function($scope, Streaks) {
   Streaks.initialise($scope, 'winning', 'winningStreaks');
 }]);
 
-streaks.controller('LosingStreaksCtrl', ['$scope', 'Streaks', function($scope, Streaks) {
+streaks.controller('LosingStreaksCtrl', ['$scope', 'Streaks',
+function($scope, Streaks) {
   Streaks.initialise($scope, 'losing', 'losingStreaks');
 }]);
