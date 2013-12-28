ff2a4bf9346a2a7aad8c1dfd6b8ca9ad3d6a1867
Alex Jones <alex.jones@unclealex.co.uk>
2013-12-28 18:48:32 +0000

    Add a second average graph for head to heads.

--- app/assets/javascripts/modules/headtoheads.js
+++ app/assets/javascripts/modules/headtoheads.js
@@ -3,10 +3,9 @@ var headtoheads = angular.module('rokta.headtoheads', ['rokta.events', 'rokta.pl
 // A factory to create a HeadToHead object used to create the graph series.
 headtoheads.service('HeadToHeads', [
 function() {
-  var service = {
-    create: function(players, allColours, headToHeads) {
+  var calculatePlayerInfo = function(players, allColours, headToHeads) {
       var activePlayerNames = _(headToHeads).keys().sortBy();
-      var activePlayers = 
+      var activePlayers =
         _(players).filter(function (player) { return activePlayerNames.contains(player.name); }).indexBy('name').value();
       var colours = activePlayerNames.map(function(name) {
         var player = activePlayers[name];
@@ -16,6 +15,61 @@ function() {
         playerIndiciesByName[player] = idx;
         return playerIndiciesByName;
       }, {});
+      return {
+        activePlayerNames: activePlayerNames,
+        activePlayers: activePlayers,
+        colours: colours,
+        playerIndiciesByName: playerIndiciesByName
+      };
+  };
+  var service = {
+    createAverages: function(players, allColours, headToHeads) {
+      var playerInfo = calculatePlayerInfo(players, allColours, headToHeads);
+      var activePlayerNames = playerInfo.activePlayerNames;
+      var activePlayers = playerInfo.activePlayers;
+      var colours = playerInfo.colours;
+      var playerIndiciesByName = playerInfo.playerIndiciesByName;
+      var data = [];
+      var addResults = function(player, resultType, count) {
+        var playerData = _.find(data, function(item) { return item.player == player; });
+        if (!playerData) {
+          playerData = {
+            player: player,
+            wins: 0,
+            losses: 0,
+            colour: colours[playerIndiciesByName[player]],
+            percentage: function() {
+              var percentage = 100 * this.wins / (this.wins + this.losses);
+              return parseFloat(percentage.toFixed(2))
+            }
+          };
+          data.push(playerData);
+        };
+        playerData[resultType] += count;
+      }
+      // Calculate all the head to head averages in objects and then sort by the best margin.
+      _.forIn(headToHeads, function(results, winner) {
+        _.forIn(results, function(count, loser) {
+          addResults(winner, 'wins', count);
+          addResults(loser, 'losses', count)
+        });
+      });
+      data = _.sortBy(data, function(item) { return -item.percentage(); });
+      _.forEach(data, function(item, idx) {
+        item.series = [];
+        for (var i = 0; i < idx; i++) {
+          item.series.push(0);
+        }
+        item.series.push(item.percentage());
+      })
+      return data;
+    },
+    create: function(players, allColours, headToHeads) {
+      var playerInfo = calculatePlayerInfo(players, allColours, headToHeads);
+      var activePlayerNames = playerInfo.activePlayerNames;
+      var activePlayers = playerInfo.activePlayers;
+      var colours = playerInfo.colours;
+      var playerIndiciesByName = playerInfo.playerIndiciesByName;
       var series = [];
       // Initialise the data
       activePlayerNames.forEach(function(name) {
@@ -107,11 +161,75 @@ function() {
   };
 }]);
 
+headtoheads.directive('roktaHeadtoheadAverages', [
+function() {
+  return {
+    restrict : 'A',
+    scope : {
+      roktaHeadtoheadAverages : '=',
+      roktaTitle : '@'
+    },
+    link : function($scope, elem, attrs) {
+      var drawChart = function() {
+        var headToHeads = $scope.roktaHeadtoheadAverages;
+        elem.highcharts({
+            chart: {
+                type: 'column'
+            },
+            title: {
+                text: null
+            },
+            xAxis: {
+                categories: _.map(headToHeads, 'player')
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: '%age of head to heads won'
+                },
+                stackLabels: {
+                    enabled: true,
+                    style: {
+                        fontWeight: 'bold',
+                        color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                    }
+                }
+            },
+            tooltip: {
+                formatter: function() {
+                    return '<b>'+ this.x + ' ' + this.point.stackTotal + '%</b>';
+                }
+            },
+            plotOptions: {
+                column: {
+                    stacking: 'normal',
+                    dataLabels: {
+                        enabled: false
+                    }
+                }
+            },
+            series: _.map(headToHeads, function(item) {
+              return { name: item.player, color: item.colour, data: item.series };
+            })
+        });
+      };
+      $scope.$watch('roktaHeadtoheadAverages', function(roktaHeadToHeads) {
+        if (roktaHeadToHeads) {
+          drawChart();
+        }
+      });
+    }
+  };
+}]);
+
 headtoheads.controller('HeadToHeadsCtrl', ['$log', '$scope', 'Events', 'Stats', 'Players', 'Colours', 'HeadToHeads',
 function($log, $scope, Events, Stats, Players, Colours, HeadToHeads) {
   Stats.refresh();
   Colours.refresh();
   Events.listenTo($scope, [Stats, Players, Colours], function() {
-    $scope.headToHeads = HeadToHeads.create(Players.players, Colours.colourMap, Stats.stats.headToHeads);
+    $scope.headToHeads =
+      HeadToHeads.create(Players.players, Colours.colourMap, Stats.stats.headToHeads);
+    $scope.headToHeadAverages =
+      HeadToHeads.createAverages(Players.players, Colours.colourMap, Stats.stats.headToHeads);
   });
 }]);
